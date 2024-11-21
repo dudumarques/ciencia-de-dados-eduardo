fd3076962f5ee1402bf872def01e315c38def9b0
Eduardo <107561143+dudumarques@users.noreply.github.com>
2024-11-21 16:43:56 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,7 @@
 const url ='https://raw.githubusercontent.com/dudumarques/ciencia-de-dados-eduardo/refs/heads/main/dados-globais.json';
+
+async function visualizarInformacoesGlobais(){
+    const res = await fetch(url);
+
+    const dados = await res.json();
+}
