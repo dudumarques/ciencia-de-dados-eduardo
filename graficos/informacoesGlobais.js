3b1e51b4af184c80b819b8ccb669fe35ea896de0
Eduardo <107561143+dudumarques@users.noreply.github.com>
2024-11-21 16:56:55 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,18 +1,19 @@
-const url ='https://raw.githubusercontent.com/dudumarques/ciencia-de-dados-eduardo/refs/heads/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/dudumarques/ciencia-de-dados-eduardo/refs/heads/main/dados-globais.json';
 
-async function visualizarInformacoesGlobais(){
+async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
 
     const dados = await res.json();
 
     const paragrafo = document.createElement('p');
-paragrafo.classList.add('graficos-container__texto');
-paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} 
+    paragrafo.classList.add('graficos-container__texto');
+    
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} 
 de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas 
 em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
-const container = document.getElementById('graficos-container');
-container.appendChild(paragrafo);
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 
 visualizarInformacoesGlobais();
